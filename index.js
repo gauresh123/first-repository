fd51ce5bd33a9c6ce7bb09e56cef5b4212f56d3e
Gauresh <waranggauresh@gmail.com>
2022-02-24 15:45:25 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,82 @@
+
+/*var ary = [1,2,3,4,5,6,7,8,9,0]
+
+var a = ary.slice(0,3);
+
+var b = ary.slice(3,6);
+
+var c = ary.slice(6);
+
+var d = a.join("");
+
+var e = b.join("");
+
+var f = c.join("");
+
+
+document.write("(" + d + ")" + "  " + e + "-"+ f)*/
+
+/*var arry = [10,20,30,40];
+
+var a = arry.map(test);
+
+function test(n){
+    console.log(n*n);
+}
+
+var str = ["Arya","bhavesh","Mahesh"];
+
+var b = str.map(check);
+
+function check(x){
+    if(str.[x])
+}*/
+/*var a = [10,20,30,40];
+function check(a,b){
+    if(a[0]==b){
+        document.write(a[0]);
+        break;
+    }
+    if(a[1]==b){
+        document.write(a[1]);
+        break;
+    }
+    if(a[2]==b){
+        document.write(a[2]);
+        break;
+    }
+    if(a[3]==b){
+        document.write(a[3]);
+    }
+    else{
+        document.write("-1");
+    }
+}
+
+check(10);*/
+
+/*var arry = [10,20,30,40];
+
+function check(arry,b){
+    if(arry[0]==b){
+        console.log(0);
+
+    }else if(arry[1]==b){
+        console.log(1);
+
+    }else if(arry[2]==b){
+        console.log(2);
+
+    }else if(arry[3]==b){
+        console.log(3);
+
+    }else{
+        console.log(-1);
+    }
+}
+*/
+var n = 12;
+
+for(var a=n;a<=1;a--){
+    document.write(a);
+}
